8f42fe6b51f3f0865b77822829824f3d731d0dd1
EternalII <Lior_acolyte@walla.com>
2022-12-28 17:26:28 +0200

    Added interfaces, classes and fuel enum
    Interfaces: Fuel, Car
    Classes: Vehicle, Car
    Added fuel enum

---
+++ A23 Ex03 LiorGordon 308140631 OmriNeemani 313323685/A23 Ex03 Lior 308140631 Omri 313323685/Ex03.ConsoleUI/Console.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    class Console
+    {
+        public static void Main()
+        {
+
+        }
+    }
+
+    class RegisterVehicle
+    {
+        public string enterLicense()
+        {
+            System.Console.WriteLine("To enter vehicle into garage, please enter license number:");
+            string license = System.Console.ReadLine();
+
+            return license;
+        }
+    }
+}
---
+++ A23 Ex03 LiorGordon 308140631 OmriNeemani 313323685/A23 Ex03 Lior 308140631 Omri 313323685/Ex03.GarageLogic/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    class Class1
+    {
+
+
+    }
+
+    class WheelsData
+    {
+        string manufacturer;
+        float currAirPressure;
+        float maxAirPressure;
+
+        //fillAir(float amount)
+        //{
+
+        //    return fillng;
+        //}
+    }
+
+    public abstract class Vehicle
+    {
+        string modelName;
+        string licensePlate;
+        float energyPercentage;
+        WheelsData wheelsData = new WheelsData();
+    }
+
+    public enum FuelTypes
+    {
+        Soler,
+        Octan95,
+        Octan96,
+        Octan98
+    }
+    interface IFuel
+    {
+        FuelTypes fuelType { get; set; }
+        float currLiters { get; }
+        float maxLiters { get; }
+
+        //refuel(float amount)
+        //{
+
+        //}
+    }
+
+    interface IElectric
+    {
+        string remainingBatteryTime { get; }
+        string maxBatteryTime { get; }
+
+        //recharge(float timeAdd)
+        //{
+
+        //}
+    }
+
+    public class Car : Vehicle, IFuel // missing IElectric
+    {
+        private float fuelAmount;
+        public float currLiters{
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        private FuelTypes fType;
+
+        public FuelTypes fuelType
+        {
+            get { return fType; }
+            set { fType = value;  }
+        }
+
+        private float maxFuel;
+
+        public float maxLiters
+        {
+            get { return maxLiters; }
+            set { maxLiters = value; }
+        }
+    }
+}
+
