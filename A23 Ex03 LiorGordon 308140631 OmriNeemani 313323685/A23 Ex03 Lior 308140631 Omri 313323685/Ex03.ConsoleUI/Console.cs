010dcf6cc320edc437402b60e93c2c84867652bf
EternalII <Lior_acolyte@walla.com>
2022-12-31 05:08:29 +0200

    Extended code

--- A23 Ex03 LiorGordon 308140631 OmriNeemani 313323685/A23 Ex03 Lior 308140631 Omri 313323685/Ex03.ConsoleUI/Console.cs
+++ A23 Ex03 LiorGordon 308140631 OmriNeemani 313323685/A23 Ex03 Lior 308140631 Omri 313323685/Ex03.ConsoleUI/Console.cs
@@ -11,37 +11,135 @@ namespace Ex03.ConsoleUI
     {
         public static void Main()
         {
+            Dictionary<string, object> vehList = new Dictionary<string, object>();
             RegisterVehicle clientVehicle = new RegisterVehicle();
 
-            string license = clientVehicle.enterLicense();
+            string license = clientVehicle.enterLicense(vehList);
+
 
         }
     }
 
     class RegisterVehicle
     {
-        public string enterLicense()
+        public string enterLicense(Dictionary<string, object> vehList)
         {
-            garageVehicle data;
-            Garage licenseList = Garage.listVehicles;
-
             System.Console.WriteLine("To enter vehicle into garage, please enter license number:");
             string license = System.Console.ReadLine();
 
-            bool inGarage = licenseList.isVehInGarage(license);
-            if (inGarage)
+            if (!vehList.ContainsKey(license))
             {
-                System.Console.WriteLine("Vehicle already in garage, loading data . . .");
+                object vehicle = fillVehDetails(license);
+                try
+                {
+                    vehList.Add(license, vehicle);
+                }
+                catch (ArgumentException)
+                {
+                    System.Console.WriteLine("An element with license number = \"license\" already exists.");
+                }
             }
             else
             {
-                System.Console.WriteLine("Adding vehicle to the garage");
-                //Add details about vehicle here
+                System.Console.WriteLine("Already in system");
             }
 
+
+            //bool inGarage = licenseList.isVehInGarage(license);
+            //if (inGarage)
+            //{
+            //    System.Console.WriteLine("Vehicle already in garage, loading data . . .");
+            //}
+            //else
+            //{
+            //    System.Console.WriteLine("Adding vehicle to the garage");
+            //    //Add details about vehicle here
+            //}
             return license;
         }
 
+        public object fillVehDetails(string license)
+        {
+            var vehDetails = new Car(); //Will be a car by default
+            // Ask user for vehicle type: bike, car, truck, etc...
+            var allVehTypes = Enum.GetValues(typeof(VehicleTypes));
+            System.Console.WriteLine(Environment.NewLine + "Recognized vehicle types: ");
+            foreach (var val in allVehTypes) // prints all available vehicle types reconigzed by garage
+            {
+                System.Console.Write(val+" ");
+            }
+            System.Console.WriteLine(Environment.NewLine + Environment.NewLine + "Enter vehicle type: ");
+            string vehType = System.Console.ReadLine();
+            vehType = vehType.ToLower();
 
+            bool correctInputFlag = false;
+            while (correctInputFlag == false) {
+                if (Enum.IsDefined(typeof(VehicleTypes), vehType))
+                {
+                    correctInputFlag = true;
+                }
+                else // user has put incorrect vehicle type info that's not in the category
+                {
+                    System.Console.WriteLine("Incorrect vehicle type, try again: ");
+                    vehType = System.Console.ReadLine();
+                    vehType = vehType.ToLower();
+                }
+            }
+
+            // move part below to Garage.cs
+            switch (vehType)
+            {
+                case "bike":
+                    break;
+                case "elecbike":
+                    break;
+                case "car":
+                    vehDetails = new Car();
+                    vehDetails.currLiters = currFuel();
+                    break;
+                case "eleccar":
+                    vehDetails = new Car();
+                    vehDetails.remainingBatteryTime = currBatTime();
+                    break;
+                case "truck":
+                    break;
+            }
+
+            return vehDetails;
+        }
+
+
+        public float currFuel()
+        {
+            System.Console.WriteLine("Current amount of fuel left:");
+            string sCurrFuel = System.Console.ReadLine();
+            float.TryParse(sCurrFuel, out float currFuel);
+
+            return currFuel;
+        }
+
+        public string currBatTime()
+        {
+            System.Console.WriteLine("Current amount of battery time left in hours:");
+            string currBatTime = System.Console.ReadLine();
+
+            return currBatTime;
+        }
+        public float askToRefill()
+        {
+            System.Console.WriteLine("Amount to refill:");
+            string sAddedFuel = System.Console.ReadLine();
+            float.TryParse(sAddedFuel, out float addedFuel);
+
+            return addedFuel;
+        }
+
+        public string askToRecharge()
+        {
+            System.Console.WriteLine("Amount to recharge:");
+            string addBatTime = System.Console.ReadLine();
+
+            return addBatTime;
+        }
     }
 }
--- A23 Ex03 LiorGordon 308140631 OmriNeemani 313323685/A23 Ex03 Lior 308140631 Omri 313323685/Ex03.GarageLogic/Garage.cs
+++ A23 Ex03 LiorGordon 308140631 OmriNeemani 313323685/A23 Ex03 Lior 308140631 Omri 313323685/Ex03.GarageLogic/Garage.cs
@@ -6,34 +6,50 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    public class Garage //This is a singletone so we can only have one dictionary of licenses in garage
+    public enum FuelTypes
     {
-        private static Garage garageList = new Garage();
-        public static Garage listVehicles
-        {
-            get { return garageList; }
-        }
-        
-        private Dictionary<string, garageVehicle> licenseList = new Dictionary<string, garageVehicle>();
-        private Garage()
-        {
-
-        }
-
-        public bool isVehInGarage(string license)
-        {
-            garageVehicle data;
-            bool licenseExists = licenseList.TryGetValue(license, out data);
-            if (licenseExists)
-            {
-                return false;
-            }
-
-            return true;
-        }
+        soler,
+        octan95,
+        octan96,
+        octan98
     }
 
-    public class garageVehicle
+    public enum VehicleTypes
+    {
+        bike,
+        elecbike,
+        car,
+        eleccar,
+        truck
+    }
+    //public class Garage //This is a singletone so we can only have one dictionary of licenses in garage
+    //{
+    //    private static Garage garageList = new Garage();
+    //    public static Garage listVehicles
+    //    {
+    //        get { return garageList; }
+    //    }
+
+    //    private Dictionary<string, garageVehicle> licenseList = new Dictionary<string, garageVehicle>();
+    //    private Garage()
+    //    {
+
+    //    }
+
+    //    public bool isVehInGarage(string license)
+    //    {
+    //        garageVehicle data;
+    //        bool licenseExists = licenseList.TryGetValue(license, out data);
+    //        if (licenseExists)
+    //        {
+    //            return false;
+    //        }
+
+    //        return true;
+    //    }
+    //}
+
+    public class garageVehicleStatus
     {
         string ownerName;
         int ownerPhone;
@@ -55,63 +71,95 @@ namespace Ex03.GarageLogic
     public abstract class Vehicle
     {
         string modelName;
-        string licensePlate;
+        string licensePlaCate;
         float energyPercentage;
         WheelsData wheelsData = new WheelsData();
     }
-
-    public enum FuelTypes
-    {
-        Soler,
-        Octan95,
-        Octan96,
-        Octan98
-    }
     interface IFuel
     {
-        FuelTypes fuelType { get; set; }
-        float currLiters { get; }
+        FuelTypes fuelType { get; } // removed set
+        float currLiters { get; set; }
         float maxLiters { get; }
 
-        //refuel(float amount)
-        //{
-
-        //}
+        void refill(float amount);
     }
 
     interface IElectric
     {
-        string remainingBatteryTime { get; }
+        string remainingBatteryTime { get; set; }
         string maxBatteryTime { get; }
 
-        //recharge(float timeAdd)
-        //{
-
-        //}
+        void recharge(string timeAdd);
     }
 
-    public class Car : Vehicle, IFuel // missing IElectric
+    public class Car : Vehicle, IFuel, IElectric
     {
-        private float fuelAmount;
-        public float currLiters{
+
+        // for regular fuel
+        private FuelTypes fType;
+        public FuelTypes fuelType
+        {
+            get { return fType; }
+            // set { fType = value;  }
+        }
+
+        private float fuelAmount; // current fuel amount
+        public float currLiters
+        {
             get { return fuelAmount; }
             set { fuelAmount = value; }
         }
 
-        private FuelTypes fType;
-
-        public FuelTypes fuelType
-        {
-            get { return fType; }
-            set { fType = value;  }
-        }
-
-        private float maxFuel;
-
+        private float mFuel; // max fuel amount
         public float maxLiters
         {
-            get { return maxLiters; }
-            set { maxLiters = value; }
+            get { return mFuel; }
+        }
+
+        public void refill(float liters)
+        {
+            float maxFuel = maxLiters;
+            float currFuel = fuelAmount;
+
+            float diff = maxFuel - (currFuel + liters);
+            if (diff <= 0)
+            {
+                fuelAmount = maxFuel;
+            }
+            else
+            {
+                fuelAmount = currFuel + liters;
+            }
+        }
+
+        // for battery car
+        private string batTime;
+        public string remainingBatteryTime
+        {
+            get { return batTime; }
+            set { batTime = value; }
+        }
+
+        private string maxBatTime;
+        public string maxBatteryTime
+        {
+            get { return maxBatTime; }
+        }
+        public void recharge(string timeAdd)
+        {
+            Int32.TryParse(timeAdd, out int intTimeAdd);
+            Int32.TryParse(maxBatTime, out int intMaxBat);
+            Int32.TryParse(batTime, out int currBatTime);
+
+            float diff = intMaxBat - (currBatTime + intTimeAdd);
+            if (diff <= 0)
+            {
+                remainingBatteryTime = intMaxBat.ToString();
+            }
+            else
+            {
+                remainingBatteryTime = (currBatTime + intTimeAdd).ToString();
+            }
         }
     }
 }
